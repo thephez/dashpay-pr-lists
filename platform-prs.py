e6042004a1fa030c2532303b4cb641ae714437e6
thephez <thephez@users.noreply.github.com>
2024-09-09 09:40:36 -0400

    chore: update platform-prs.py
    Change to v1.3

--- platform-prs.py
+++ platform-prs.py
@@ -2,7 +2,7 @@ import requests
 
 ORG = 'dashevo'
 STATE = 'all'
-MILESTONE='9' # 9 = v1.0; find on https://github.com/dashevo/platform/milestones
+MILESTONE='16' # 16 = v1.3; find on https://github.com/dashevo/platform/milestones
 
 def get_pull_requests(org, repo, milestone, state='all'):
     query_url = 'https://api.github.com/repos/{}/{}/issues?&state={}&milestone={}&sort=created&direction=asc'.format(org, repo, state, milestone)
