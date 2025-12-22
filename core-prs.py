3c7a727218a849505e2ba6c7a2f6958d5c9033bf
thephez <thephez@users.noreply.github.com>
2025-12-22 16:51:44 -0500

    chore: update core milestone
    v23.1

--- core-prs.py
+++ core-prs.py
@@ -2,7 +2,7 @@ import requests
 
 ORG = 'dashpay'
 REPO = 'dash'
-MILESTONE = '52' # 52 = v23.0; find on https://github.com/dashpay/dash/milestones
+MILESTONE = '59' # 59 = v23.1; find on https://github.com/dashpay/dash/milestones
 STATE = 'all'
 
 
